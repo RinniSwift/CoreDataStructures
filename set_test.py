fc8dd78293cf75b022e505404ea24117be065cdb
Rinni Swift <rinradaswift@gmail.com>
2019-04-23 18:38:24 -0700

    added starter set test

---
+++ set_test.py
@@ -0,0 +1,44 @@
+
+from set import Set
+import unittest
+
+class SetTest(unittest.TestCase):
+
+	def test_init(self):
+		s = Set()
+		assert s.count == 0
+		s_two = Set(['R', 'I', 'N'])
+		assert s_two.count == 3
+
+	def test_contains(self):
+		s = Set(['A', 'B', 'C'])
+		assert s.contains('A') == True
+		assert s.contains('B') == True
+		assert s.contains('C') == True
+		assert s.contains('D') == False
+		assert s.contains('E') == False
+		assert s.count == 3
+
+	def test_add(self):
+		s = Set()
+		s.add('R')
+		assert s.count == 1
+		assert s.contains('R') == True
+		s.add('I')
+		assert s.count == 2
+		assert s.contains('I') == True
+		s.add('I')
+		assert s.count == 2
+		assert s.contains('I') == True
+
+		s_two = Set(['R', 'I', 'N'])
+		s_two.add('N')
+		assert s_two.count == 3
+		s_two.add('S')
+		assert s_two.count == 4
+		s_two.add('A')
+		assert s_two.count == 5
+		s_two.add('R')
+		assert s_two.count == 5
+
+		
