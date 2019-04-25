eff74b259dfb01419bfc09321edf50cfc95e33b6
Rinni Swift <rinradaswift@gmail.com>
2019-04-25 13:20:06 -0700

    added new implementation of test cases for circular buffer

--- circular_buffer_test.py
+++ circular_buffer_test.py
@@ -8,12 +8,11 @@ class CircularBufferTest(unittest.TestCase):
 		c = CircularBuffer()
 		assert c.size == 0
 		assert c.max_size == 12
-		assert c.elements == []
-
-		c = CircularBuffer(18)
+		assert c.elements == [None, None, None, None, None, None, None, None, None, None, None, None]
+		c = CircularBuffer(4)
 		assert c.size == 0
-		assert c.max_size == 18
-		assert c.elements == []
+		assert c.max_size == 4
+		assert c.elements == [None, None, None, None]
 
 	def test_is_empty(self):
 		c = CircularBuffer()
@@ -37,28 +36,60 @@ class CircularBufferTest(unittest.TestCase):
 		assert c.size == 4
 
 	def test_enqueue(self):
-		c = CircularBuffer()
+		c = CircularBuffer(6)
 		assert c.size == 0
-		assert c.elements == []
+		assert c.elements == [None, None, None, None, None, None]
 		c.enqueue('R')
 		c.enqueue('I')
 		c.enqueue('N')
-		assert c.elements == ['R', 'I', 'N']
+		assert c.elements == [None, None, None, 'R', 'I', 'N']
+		assert c.size == 3
+		new = CircularBuffer(4)
+		new.enqueue('A')
+		new.enqueue('B')
+		new.enqueue('C')
+		new.enqueue('D')
+		assert new.elements == ['A', 'B', 'C', 'D']
+		assert new.is_full() == True
+		assert new.size == 4
+		new.enqueue('E')
+		assert new.elements == ['B', 'C', 'D', 'E']
+		assert new.size == 4
 
 	def test_dequeue(self):
-		c = CircularBuffer()
+		c = CircularBuffer(6)
 		c.enqueue('R')
 		c.enqueue('I')
 		c.enqueue('N')
-		assert c.elements == ['R', 'I', 'N']
+		assert c.size == 3
+		assert c.elements == [None, None, None, 'R', 'I', 'N']
 		assert c.dequeue() == 'R'
 		assert c.size == 2
-		assert c.elements == ['I', 'N']
+		assert c.elements == [None, None, None, None, 'I', 'N']
 		assert c.dequeue() == 'I' 
 		assert c.size == 1
-		assert c.elements == ['N']
+		assert c.elements == [None, None, None, None, None, 'N']
+		assert c.is_empty() == False
 		assert c.dequeue() == 'N'
 		assert c.size == 0
+		assert c.elements == [None, None, None, None, None, None]
 		assert c.dequeue() == None
 		assert c.size == 0
+		assert c.is_empty() == True
+
+		new = CircularBuffer(4)
+		new.enqueue('A')
+		new.enqueue('B')
+		new.enqueue('C')
+		new.enqueue('D')
+		assert c.elements == ['A', 'B', 'C', 'D']
+		assert new.dequeue() == 'A'
+		assert c.elements == [None, 'B', 'C', 'D']
+		new.enqueue('E')
+		new.enqueue('F')
+		assert new.elements == ['C', 'D', 'E', 'F']
+		assert new.dequeue() == 'C'
+		assert new.dequeue() == 'D'
+		assert new.elements == [None, None, 'E', 'F']
+
 
