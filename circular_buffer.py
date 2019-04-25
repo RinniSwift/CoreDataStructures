5cd33ba8fdac18715266243f548099db88017c56
Rinni Swift <rinradaswift@gmail.com>
2019-04-25 15:13:49 -0700

    updated all functions and passed all test cases

--- circular_buffer.py
+++ circular_buffer.py
@@ -3,7 +3,7 @@ class CircularBuffer(object):
 	def __init__(self, max_size=12):
 		self.size = 0
 		self.max_size = max_size
-		self.elements = []
+		self.elements = [None]*max_size
 
 	def is_empty(self):
 		if self.size == 0:
@@ -18,13 +18,30 @@ class CircularBuffer(object):
 			return False
 
 	def enqueue(self,item):
-		if self.is_full() is False:
-			self.elements.append(item)
+		"""insert at back of the array"""
+
+		if self.is_empty():
+			# if buffer is empty, set the last element to be the item
+			self.elements[-1] = item
+		else:
+			# shift all items to the left of it's origianl place
+			index = 0
+			while index < self.max_size - 1:
+				self.elements[index] = self.elements[index + 1]
+				index += 1
+			self.elements[-1] = item
+
+		# check if we are adding items to the array or it's simply full and we're just shifting items off
+		if self.size < self.max_size:
 			self.size += 1
+		
+
 
 	def front(self):
 		if self.size > 0:
-			return self.elements[0]
+			for item in self.elements:
+				if item != None:
+					return item
 		else:
 			return None
 		
@@ -32,7 +49,10 @@ class CircularBuffer(object):
 		if self.size == 0:
 			return None
 		else:
-			first = self.elements[0]
-			self.elements.remove(first)
+			index = self.max_size - self.size
+			front = self.elements[index]
+			self.elements[index] = None
 			self.size -= 1
-			return first
+			print("front: {}".format(front))
+			return front
+
