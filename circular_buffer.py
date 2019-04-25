27e88f3414671f2a9e454e3519d80cc3d84e975b
Rinni Swift <rinradaswift@gmail.com>
2019-04-25 15:31:38 -0700

    added comments for all functions

--- circular_buffer.py
+++ circular_buffer.py
@@ -18,7 +18,7 @@ class CircularBuffer(object):
 			return False
 
 	def enqueue(self,item):
-		"""insert at back of the array"""
+		"""push item from back of the array"""
 
 		if self.is_empty():
 			# if buffer is empty, set the last element to be the item
@@ -27,32 +27,34 @@ class CircularBuffer(object):
 			# shift all items to the left of it's origianl place
 			index = 0
 			while index < self.max_size - 1:
+				# setting the current index to be that of the item on the right
 				self.elements[index] = self.elements[index + 1]
 				index += 1
+			# set the added items back to be the item
 			self.elements[-1] = item
 
-		# check if we are adding items to the array or it's simply full and we're just shifting items off
+		# check if we are adding items to the array or simply shifting items off
 		if self.size < self.max_size:
 			self.size += 1
-		
-
 
 	def front(self):
 		if self.size > 0:
-			for item in self.elements:
-				if item != None:
-					return item
+			# calculate first item index by subtracting the total size by the amount of items in the array
+			index = self.max_size - self.size
+			return self.elements[index]
 		else:
+			# no items
 			return None
 		
 	def dequeue(self):
+		# no items to dequeue
 		if self.size == 0:
 			return None
 		else:
+			# calculate the first index and set to None
 			index = self.max_size - self.size
 			front = self.elements[index]
 			self.elements[index] = None
 			self.size -= 1
-			print("front: {}".format(front))
 			return front
 
